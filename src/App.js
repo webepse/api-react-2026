8dee87a8a5d0422dc2cf80d5446beeea0b6f83ea
Boderio <berti@epse.be>
2026-01-21 11:33:26 +0100

    Axios V1 (react devtools)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
+import CustomersPage from "./pages/CustomersPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
             <main className="container pt-5">
                 <Routes>
                     <Route path="/" element={<HomePage />} />
+                    <Route path="/customers" element={<CustomersPage />} />
                 </Routes>
             </main>
         </Router>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
+import {NavLink, useNavigate} from "react-router-dom";
+
 const Navbar = (props) => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">API-Platform React</a>
+                <NavLink className="navbar-brand" to="/">API-Platform React</NavLink>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -10,7 +12,7 @@ const Navbar = (props) => {
                 <div className="collapse navbar-collapse" id="navbarColor01">
                     <ul className="navbar-nav me-auto">
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Clients</a>
+                            <NavLink className="nav-link" to="/customers">Clients</NavLink>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="#">Factures</a>
