0d213d1a3f398d3c106cdc290263aa9562fa36dd
Boderio <berti@epse.be>
2026-01-21 11:45:39 +0100

    Axios V2 - affichage

--- src/pages/CustomersPage.jsx
+++ src/pages/CustomersPage.jsx
@@ -11,9 +11,43 @@ const CustomersPage = (props) => {
             .catch(error => console.log(error.response));
     },[])
 
+
+
     return(
         <>
-
+            <h1>Liste des clients</h1>
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Client</th>
+                        <th>E-mail</th>
+                        <th>Entreprise</th>
+                        <th className="text-center">Factures</th>
+                        <th className="text-center">Montant total</th>
+                        <th className="text-center">Montant restant</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                {customers.map(customer => (
+                    <tr key={customer.id}>
+                        <td>{customer.id}</td>
+                        <td>{customer.firstName} {customer.lastName}</td>
+                        <td>{customer.email}</td>
+                        <td>{customer.company}</td>
+                        <td className="text-center">
+                            <span className="badge text-bg-primary">{customer.invoices.length}</span>
+                        </td>
+                        <td className="text-center">{customer.totalAmount.toLocaleString()}</td>
+                        <td className="text-center">{customer.unpaidAmount.toLocaleString()}</td>
+                        <td>
+                            <button className="btn btn-sm btn-danger">Supprimer</button>
+                        </td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
         </>
     )
 }
